927402b6ee9ff163168d5067c66523cdbf080533
Ayaat2012 <iffatrajshahi@gmail.com>
2025-07-19 20:33:07 +0600

    initial commit

---
+++ Project-2.py
@@ -0,0 +1,12 @@
+# Taking inputs from user
+x = input("Enter value of x :")
+y = input("Enter value of y :")
+z = input("Enter value of z :")
+
+# Single line swap
+x,y,z = y,z,x = z,x,y
+
+# Displaying results aafter swapping
+print("Value of x after swapping :", x)
+print("Value of y after swapping :", y)
+print("Value of z after swapping :", z)
